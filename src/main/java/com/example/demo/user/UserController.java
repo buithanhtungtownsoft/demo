25a5a94b5bf4d238e3b22fe1b0d394a915407238
tungbt <bui.thanh.tung@townsoftvina.com>
2025-06-26 11:51:24 +0700

    update

--- src/main/java/com/example/demo/user/UserController.java
+++ src/main/java/com/example/demo/user/UserController.java
@@ -3,9 +3,13 @@ package com.example.demo.user;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @RestController
 @Slf4j
@@ -18,6 +22,14 @@ public class UserController {
     @GetMapping("/user")
     public Flux<UserModel> getUser() {
         return UserService.getAllUsers();
+    }
+
+    @GetMapping("/me")
+    public Mono<UserModel> getCurrentUser() {
+        return ReactiveSecurityContextHolder.getContext()
+                .map(SecurityContext::getAuthentication)
+                .map(Authentication::getPrincipal)
+                .cast(UserModel.class);
     }
 
 }
